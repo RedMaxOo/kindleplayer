f38ad75c8ff3fd1071b9c1cbca51a50af353e2f5
malongqi <417072415@qq.com>
2018-06-07 23:07:22 +0800

    新增index

---
+++ src/view/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <x-header></x-header>
+  </div>
+</template>
+<script>
+  import XHeader from './header.vue'
+  export default  {
+    components: {XHeader},
+    data () {
+      return {}
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+  }
+</script>
